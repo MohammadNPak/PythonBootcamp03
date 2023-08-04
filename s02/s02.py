73b19635621dc452d1707101318484e4407b71b7
mohammadnpak <m.nozaripak@gmail.com>
2023-08-04 09:16:47 +0330

    second commit

---
+++ s02/s02.py
@@ -0,0 +1,114 @@
+# if شرط:
+#     دستورات
+# indent
+
+# if (شرط){
+# دستورات
+# }
+# x = int(input("x:"))
+# if x < 10:
+#     print('x <10')
+#     print("salam")
+
+
+# print("end of if")
+# A comparison operator compares two values and returns a boolean value, either True or False
+# <   a = 10<9
+# >   a = 10>10
+# <=  a = 10<=9
+# >=  a = 10>=10
+# !=  a = 10!=9
+# ==  a = 10==9
+
+
+# age = int(input("enter your age: "))
+# if age <= 12:
+#     print("kooddak")
+# else:
+#     if age<=18:
+#         print("nojavan")
+#     else:
+#         if age<=25:
+#             print("javan")
+#         else:
+#             print("bozorsal")
+
+# <12 kooddak
+# < 18 nojavan
+# <25 javan
+# bozorsal
+
+
+# age = int(input("enter your age: "))
+# if age <= 12:
+#     print("kooddak")
+# elif age<=18:
+#     print("nojavan")
+# elif age<=25:
+#     print("javan")
+# else:
+#     print("bozorsal")
+
+
+# mean = float(input())
+
+# if 0 <= mean <= 20:
+#     if mean < 10:
+#         print("very bad")
+#     elif mean < 15:
+#         print("normal")
+#     elif mean < 18:
+#         print("good")
+#     else:
+#         print("excelent")
+# else:
+#     print("invalid input!")
+
+
+# and or not
+# bool
+
+# x       y       x and y     x or y      not x
+# False   False   False       False       True
+# False   True    False       True        True
+# True    False   False       True        False
+# True    True    True        True        False
+
+
+# mean = float(input())
+
+# if 0 > mean or mean > 20:
+#     print("invalid input")
+# elif mean < 10:
+#     print("very bad")
+# elif mean < 15:
+#     print("normal")
+# elif mean < 18:
+#     print("good")
+# else:
+#     print("excelent")
+
+
+# while شرط:
+#     دستورات
+
+# i = 0
+# while i<10:
+#     print(i)
+#     i=i+1
+
+
+s=0
+i = 0
+while i<=7:
+    # print(i)
+    s=s+i
+    i=i+1
+print(s)
+
+
+
+
+
+
+
