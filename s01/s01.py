e168291ee0864e2118a754fe49bbcb5c1c9d01b0
alireza-bamdad <mkalireza.ir@gmail.com>
2023-08-04 09:12:25 +0330

    first commit

---
+++ s01/s01.py
@@ -0,0 +1,96 @@
+# print("salam")
+# print("mohammad Nozari pak")
+
+# 1GB = 1024*MB = 1024*1024*KB=1024*1024*1024Bytes = 1024*1024*1024*8 bit
+
+
+# year =20
+# month = 2
+# days = 24
+# d = year*365 + month*30 + days
+# print(d) 
+
+# name = "mohammad"
+# last_name = "Nozari"
+# print(name,last_name)
+
+# variable
+# str       "mohammad" "tehran" "10"  "c"
+# number
+    # bool      True False
+    # int       1 0 -4 100003
+    # float     3.14 -2.7 0.0 1.0
+    # complex   2.2+6j
+    
+# +-*/   ** // %
+
+# print(2.0+3)
+# print(4/2)
+# print(2*3)
+# print(2-3)
+
+# 2**3
+# 2**0.5
+
+
+# first_name  = input("please enter your name:")
+# age = 20
+# print("your name was",first_name)
+# print(type(first_name))
+# print(type(age))
+# print(id(age))
+
+# a-z A-Z _ 0-9   first_name first name  first@name  first1_name1 
+# 1name ->  _name name
+# print = 2
+
+
+# a = "True"
+# b = bool(a)
+# print(b)
+# c = 10
+# d = str(c) #-> "10"
+
+# print(int(False))
+# print(ord("d"))
+# print(chr(100))
+
+
+# year = int(input("please enter year: "))
+# month = int(input("please enter month: "))
+# day = int(input("please enter day: "))
+
+# print("days:",year*365+month*30+day)
+# d = year*365+month*30+day
+# print(f"you are {d} days old:")
+
+
+
+# a = float(input("please enter a: "))
+# b = float(input("please enter b: "))
+# c = float(input("please enter c: "))
+# # a=1
+# # b=2
+# # c=1
+# x1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
+    # (-b+(4-4*a*c)**0.5)/(2*a)
+    # (-b+(4-4*c)**0.5)/(2*a)
+    # (-b+(4-4)**0.5)/(2*a)
+    # (-b+0**0.5)/(2*a)
+    # (-b+0)/(2*a)
+    # (-2+0)/(2*a)
+    # -2/(2*a)
+    # -2/(2*1)
+    # -2/2
+    # -1
+# ()
+# **
+# */
+# +-
+
+
+
+# from math import *
+# from math import pi,sin
+import math
+print(math.sin(math.radians(90)))
