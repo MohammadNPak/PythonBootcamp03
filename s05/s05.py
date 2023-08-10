f98a3fa723478676023b4fbde35049bcc9180b06
mohammadnpak <m.nozaripak@gmail.com>
2023-08-10 09:32:08 +0330

    s05-b

--- s05/s05.py
+++ s05/s05.py
@@ -10,20 +10,51 @@
         
 # split_str(m,"seperator")
 
-s = "salam sep bye sep goodnight"
-a = s.split("sep")
-print(a)
+# s = "salam sep bye sep goodnight"
+# a = s.split("sep")
+# print(a)
 
 
 m = "seperatorshanbe seperator 1shanbeseperator 2shanbe seperator3shanfgfrbe"
 
-def split_arian(s, char):
-    start = 0
-    end = 0
-    while end < len(s):
-        if s[end:end+len(char)] == char or end == len(s)-1:
-            print(s[start:end])
-            start = end + len(char)
-        end += 1
+# def split_arian(s, char):
+#     start = 0
+#     end = 0
+#     while end < len(s):
+#         if s[end:end+len(char)] == char or end == len(s)-1:
+#             print(s[start:end])
+#             start = end + len(char)
+#         end += 1
 
-split_str(m, "seperator")
+# split_str(m, "seperator")
+
+
+name = "mohmmad"
+a = name[1:4]
+
+# names = ["mohmmad","reza","arian",1,3.14,True,]
+# # index    0          1       2   3 4    5
+# print(len(names))
+# print(names[5])
+# names[5] = 10
+# print(names)
+# names.append("lksdjhfkl")
+# print(names)
+# names.pop(2)
+# print(names)
+# names.remove("reza")
+# print(names)
+
+
+names = ["mohmmad","reza","arian",1,3.14,True]
+
+i = 0
+while i<len(names):
+    print(names[i])
+    i+=1
+    
+for i in range(len(names)):
+    print(names[i])
+    
+for x in names:
+    print(x)
